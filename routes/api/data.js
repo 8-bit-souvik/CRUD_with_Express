d5dcf87bfae849fd02a0f86cfbd9019138bf4d7b
souvik <Sonu.at.2001.20@gmail.com>
2021-07-05 21:49:55 +0530

    graphical interface added with this aplication

--- routes/api/data.js
+++ routes/api/data.js
@@ -8,26 +8,42 @@ const fs = require('fs');
 var data = require('../../dataBase/members.json').data;
 
 //returns all data
-router.get('/', (req, res) => {
+router.get('/all', (req, res) => {
     res.json(data);
 });
 
 
 //returns perticular data of requested serial number
 router.get('/:sl', (req, res) => {
-    const found = data.some(data => data.serial === parseInt(req.params.sl));
+    const foundBySerial = data.some(data => data.serial === parseInt(req.params.sl));
+    const foundByName = data.some(data => data.name === (req.params.sl));
 
-    if (found) {
+    if (foundBySerial) {
         res.json(data.filter(data => data.serial === parseInt(req.params.sl)));
     }
-    else {
-        res.status(404).json({ msg: `404 || data not found at serial number: ${req.params.sl}` });
+    else if (foundByName) {
+
+        // res.json(data.filter(data => data.name === (req.params.sl)));
+        i = 0; nums = [];
+        data.forEach(data => {
+
+            if (data.name === (req.params.sl)) {
+                i++;
+                nums.push(data.serial);
+            }
+        });
+        res.json({ msg: ` ${i} people matched with the name of ${req.params.sl} `, serial: `${nums}` });
+
+    } else {
+        res.status(404).json({ msg: `404 || data not matched with: ${req.params.sl}` });
     }
 });
 
 
 //create new data
 router.post('/', (req, res) => {
+    console.log(`${req.body}`);
+    console.log(`${req.body.name} ${req.body.age} ${req.body.email}`);
     const newData = {
         serial: data.length + 1,
         name: req.body.name,
@@ -37,18 +53,21 @@ router.post('/', (req, res) => {
         joined: Date(),
         id: uuid.v4()
     }
-    if (!newData.name || !newData.age || !newData.email) {
+
+    if (!req.body.name || !req.body.age || !req.body.email) {
         return res.status(400).json({ msg: 'Please include name, age and email' })
     }
     data.push(newData);
     res.json({ msg: 'data pushed', newData });
 
-    fs.writeFileSync('../dataBase/members.json', `${JSON.stringify({data}, null, 1)}`);
+    fs.writeFileSync('../dataBase/members.json', `${JSON.stringify({ data }, null, 1)}`);
 })
 
 
 // Update a data
 router.put('/:sl', (req, res) => {
+    console.log(`${req.body}`);
+    console.log(`${req.body.name} ${req.body.age} ${req.body.email}`);
     const found = data.some(data => data.serial === parseInt(req.params.sl));
 
     if (found) {
@@ -69,7 +88,7 @@ router.put('/:sl', (req, res) => {
             }
         });
 
-        fs.writeFileSync('../dataBase/members.json', `${JSON.stringify({data}, null, 1)}`);
+        fs.writeFileSync('../dataBase/members.json', `${JSON.stringify({ data }, null, 1)}`);
     }
     else {
         res.status(400).json({ msg: `400 || data not found at serial number: ${req.params.sl}` });
@@ -79,32 +98,34 @@ router.put('/:sl', (req, res) => {
 
 // Delete a data
 router.delete('/:sl', (req, res) => {
-    const found = data.some(data => data.serial === parseInt(req.params.sl));
+    const found = data.some(data => data.serial === parseInt(req.params.sl) && data.name === (req.body.name));
 
     if (found) {
-        
-      //  data = data.filter(data => data.serial !== parseInt(req.params.sl));
 
-      data.forEach(data => {
-          if (data.serial === parseInt(req.params.sl)) {
-              
-              //serial number of every member should be unique, so serial number will not be deleted due to avoid complexity.
-              data.name = undefined;
-              data.age = undefined;
-              data.email = undefined;
-              data.active = undefined; 
-              data.joined = undefined;
-              data.id = undefined;     
+        //  data = data.filter(data => data.serial !== parseInt(req.params.sl));
 
-              res.json({ msg: 'data deleted', data });
-          }
-      });
+        data.forEach(data => { console.log(data.serial);
+            if (data.serial === parseInt(req.params.sl) && data.name === (req.body.name)) {
+
+                //serial number of every member should be unique, so serial number will not be deleted due to avoid complexity.
+                data.name = undefined;
+                data.age = undefined;
+                data.email = undefined;
+                data.active = undefined;
+                data.joined = undefined;
+                data.id = undefined;
+
+                
+
+                res.json({ msg: `data deleted at serial number ${data.serial}`, data });
+            } 
+        });
 
 
-        fs.writeFileSync('../dataBase/members.json', `${JSON.stringify({data}, null, 1)}`);
+        fs.writeFileSync('../dataBase/members.json', `${JSON.stringify({ data }, null, 1)}`);
     }
     else {
-        res.status(400).json({ msg: `400 || data not found at serial number: ${req.params.sl}` });
+        res.status(400).json({ msg: `400 || data not found at serial number: ${req.params.sl} and name: ${req.body.name}` });
     }
 });
 
